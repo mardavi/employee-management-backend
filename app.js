8b97596489d901fbddf5bb49dcdf1d074af4854e
mardavi <dmar@my.centennialcollege.ca>
2024-11-25 12:53:55 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+mongoose
+    .connect("mongodb+srv://mardavi:vIyTXKpLrR3xIJjR@cluster0.ntplj.mongodb.net/employee-management?retryWrites=true&w=majority&appName=Cluster0")
+    .then(() => {
+        app.listen(8080);
+    })
+    .catch(err => {
+        console.log(err);
+    })
