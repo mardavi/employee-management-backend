943d6f4c9ef353758c0dedde108694a504d69561
Tanjil bin hasan <tbh20572@gmail.com>
2024-11-25 17:38:35 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const HttpError = require("./models/http-error");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-// Tanjil middleware 
+// Middleware 
+app.use((req, res, next) => {
+    throw new HttpError("The requested URL was not found on this server.", 404);
+});
 
+//mandatory middleware to handle the error
+app.use((error, req, res, next) => {
+    if (res.headerSent) {
+        return next(error);
+    }
+
+    res.status(error.code || 500);
+    res.json({ message: error.message } || "An unknown error occurred!"); 
+
+});
 mongoose
     .connect("mongodb+srv://mardavi:vIyTXKpLrR3xIJjR@cluster0.ntplj.mongodb.net/employee-management?retryWrites=true&w=majority&appName=Cluster0")
     .then(() => {
