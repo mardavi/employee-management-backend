ded340953657da913e0f47a827c3d1d3c0cb53f6
mardavi <dmar@my.centennialcollege.ca>
2024-11-25 16:11:57 -0500

    redo test

--- controllers/employees-controllers.js
+++ controllers/employees-controllers.js
@@ -1,13 +1,1 @@
-// justine and trizha
-<<<<<<< HEAD
-const Member = {
-    name: String,
-    email: String,
-    membershipType: String,
-    startDate: Date,
-    endDate: Date
-};
-//testing!!
-=======
-//test
->>>>>>> 5ec5a8d34c4888f6cff9b5a435f2250c5371740a
+//justine and trizha
