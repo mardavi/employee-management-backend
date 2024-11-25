766975b73b83b9c4a5342381ca27122fba6b8916
bacanit <trizha.14@gmail.com>
2024-11-25 17:14:50 -0500

    Update employees-controllers.js

--- controllers/employees-controllers.js
+++ controllers/employees-controllers.js
@@ -1,2 +1,117 @@
 // justine and trizha
-//test
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const deleteEmployee = async (req, res, next) => {
+    const employeeId = req.params.eid;
+
+    let employeeInfo;
+    try{
+        employeeInfo = await Employee.findById(employeeId);
+    }catch(err){
+        return next(new HttpError("Could not find employee profile to delete.", 500));
+    }
+
+    if (!employeeInfo) {
+        return next(new HttpError("Employee profile does not exist.", 404));
+    }
+
+    try {
+        await employeeInfo.deleteOne();
+    }catch(err){
+        return next(new HttpError("Something went wrong, could not delete employee profile.", 500));
+    }
+    res.status(200).json({message: "Employee profile deleted successfully."});
+    }
+
+    const updateEmployee = async (req, res, next) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()){
+            console.log(errors);
+            return next(new HttpError("Please enter correct data.", 400))
+        }
+
+        const { name, position, department, email, phone } = req.body;
+
+        const employeeId = = req.params.pid;
+
+        
+    let employeeInfo;
+    try{
+        employeeInfo = await Employee.findById(employeeId);
+    }catch(err){
+        return next(new HttpError("Could not update employee profile.", 500));
+    }
+
+    if(!employeeInfo){
+        return next(new HttpError("Invalid employee ID. Could not find the employee profile."));
+    }
+
+    employeeInfo.name = name;
+    employeeInfo.position = position;
+    employeeInfo.department = department;
+    employeeInfo.email = email;
+    employeeInfo.phone = phone;
+
+    try {
+        await employeeInfo.save();
+    }catch(err){
+        return next(new HttpError("Something went wrong, could not employee profile", 500))
+    }
+
+    res.status(200).json({place: employeeInfo.toObject({getters:true})});
+    }
+    
+exports.getEmployeeById = getEmployeeById;
+exports.createEmployee = createEmployee;
+exports.deleteEmployee = deleteEmployee;
+exports.updateEmployee = updateEmployee;
+
+
