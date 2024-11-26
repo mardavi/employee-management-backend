d78a0105208028abf8ad5b0073d7c6008fae82c6
John Jeongho Joo <82235019+Oranginadooboo@users.noreply.github.com>
2024-11-25 20:17:22 -0500

    Update employees-routes.js

--- routes/employees-routes.js
+++ routes/employees-routes.js
@@ -1,1 +1,37 @@
 // john
+const express = require("express");
+const { check } = require("express-validator");
+
+const employeesController = require("../controllers/employees-controller");
+
+const router = express.Router();
+
+router.get("/:pid", employeesController.getEmployeeById);
+
+router.post(
+  "/",
+  [
+    check("name").not().isEmpty(),
+    check("position").not().isEmpty(),
+    check("department").not().isEmpty(),
+    check("email").isEmail(),
+    check("phone").isMobilePhone(),
+  ],
+  employeesController.createEmployee
+);
+
+router.patch(
+  "/:pid",
+  [
+    check("name").not().isEmpty(),
+    check("position").not().isEmpty(),
+    check("department").not().isEmpty(),
+    check("email").isEmail(),
+    check("phone").isMobilePhone(),
+  ],
+  employeesController.updateEmployee
+);
+
+router.delete("/:eid", employeesController.deleteEmployee);
+
+module.exports = router;
